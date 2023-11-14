e2e0961fdb837cdb7bee358d0f78685d955ab2e4
Nayiri <nayiri@berkeley.edu>
2023-11-14 11:48:38 -0800

    adding custom files

---
+++ vlsi/custom.mk
@@ -0,0 +1,13 @@
+extra               ?=  # extra configs
+args                ?=  # command-line args (including step flow control)
+
+vlsi_dir=$(abspath .)
+tech_name          = sky130
+INPUT_CONFS        = $(vlsi_dir)/custom.yml $(extra)
+ENV_YML            = $(vlsi_dir)/custom-env.yml
+
+HAMMER_EXTRA_ARGS   ?= $(foreach conf, $(INPUT_CONFS), -p $(conf)) $(args)
+
+BINARY 				?= $(RISCV)/riscv64-unknown-elf/share/riscv-tests/benchmarks/towers.riscv
+
+CONFIG				?= RocketConfig
