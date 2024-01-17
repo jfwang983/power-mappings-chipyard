8ee861c86a2baf035b2c096ece075d8cf7144ee8
Jonathan Wang <69096710+jfwang983@users.noreply.github.com>
2024-01-17 11:28:32 -0800

    Update custom.mk

--- vlsi/custom.mk
+++ vlsi/custom.mk
@@ -8,7 +8,6 @@ ENV_YML            = $(vlsi_dir)/custom-env.yml
 
 HAMMER_EXTRA_ARGS   ?= $(foreach conf, $(INPUT_CONFS), -p $(conf)) $(args)
 
-BINARY ?= /scratch/jfwang/power-prediction/power-mappings-chipyard/generators/gemmini/software/gemmini-rocc-tests/build/bareMetalC/matmul_tilings_0-baremetal
-# BINARY 				?= $(RISCV)/riscv64-unknown-elf/share/riscv-tests/benchmarks/towers.riscv
+BINARY 				?= $(RISCV)/riscv64-unknown-elf/share/riscv-tests/benchmarks/towers.riscv
 
 CONFIG				?= CustomGemminiSoCConfig
