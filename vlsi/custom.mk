518984963f6cad16a1b823ee756f4990590a7355
Jonathan Wang <jfwang983@bwrcrdsl-5.eecs.berkeley.edu>
2024-09-17 16:36:21 -0700

    Intech22 configs

--- vlsi/custom.mk
+++ vlsi/custom.mk
@@ -2,7 +2,7 @@ extra               ?=  # extra configs
 args                ?=  # command-line args (including step flow control)
 
 vlsi_dir=$(abspath .)
-tech_name          = sky130
+tech_name          = intech22
 INPUT_CONFS        = $(vlsi_dir)/custom.yml $(extra)
 ENV_YML            = $(vlsi_dir)/custom-env.yml
 
@@ -11,4 +11,4 @@ HAMMER_EXTRA_ARGS   ?= $(foreach conf, $(INPUT_CONFS), -p $(conf)) $(args)
 BINARY 				?= $(RISCV)/riscv64-unknown-elf/share/riscv-tests/benchmarks/towers.riscv
 LOADMEM 			?= $(BINARY)
 
-CONFIG				?= CustomGemminiSoCConfig
+CONFIG				?= RocketConfig
